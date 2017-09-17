793a735cf3c7eadf4b23f9cb349bcc93063126b9
Adi <adithya.kiran.sekar@gmail.com>
2017-09-17 00:40:07 -0500

    Recursion Reverse a string

---
+++ 8. Recursion and Dynamic Programming/Anagrams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI._8._Recursion_and_Dynamic_Programming
+{
+    class Anagrams
+    {
+    }
+}
---
+++ 8. Recursion and Dynamic Programming/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI._8._Recursion_and_Dynamic_Programming
+{
+    public class ReverseString
+    {
+        // str = Hello
+        public static string Reverse(string str)
+        {
+            if (str.Length <= 1)
+            {
+                return str;
+            }
+            // Take first character
+            var ch = str[0];
+            var subStr = str.Substring(1, str.Length - 1);
+            var reversedStr = Reverse(subStr) + ch;
+            return reversedStr;
+        }
+    }
+}
